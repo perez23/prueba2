b092afee0517ec036192c0c1d6c92af5227f77ba
Pablo Perez <pablux23@gmail.com>
2014-03-26 14:38:02 +0100

    creacion

---
+++ index.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html lang="es">
+<head>
+    <meta charset="utf-8" />
+    <title>xxxxx</title>
+	 <link rel="stylesheet" type="text/css" href="./css/css.css">
+	 <script type="text/javascript" src="./js/js.js"></script>
+</head>
+
+<body>
+        <header>
+            <nav>
+                <a href="inicio.html">Inicio</a>
+                <a href="nosotros.html">Nosotros</a>
+                <a href="opcion.html">Opcion</a>
+                <a href="opcion.html">Opcion</a>
+
+            </nav>
+        </header>
+        <!-- Cuerpo de la pagina -->
+        <footer>
+            
+          <div id="copyright">Copyright&copy; 2013 - 2014 Página creada por Pablo Pérez - Todos los derechos reservados</div>
+        </footer>
+
+</body>
+</html>
